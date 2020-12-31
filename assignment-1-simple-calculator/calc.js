51b01a97421ef0681251c7d074cfe10243569da9
muskan132 <muskangoyal2400@gmail.com>
2020-12-31 07:54:39 +0530

    added a function to evaluate

--- assignment-1-simple-calculator/calc.js
+++ assignment-1-simple-calculator/calc.js
@@ -25,15 +25,52 @@ function sendOP(digit) { //doubt regarding operator conditioning
  }
 
 function equalTo() {
-	for(i=0; i<num.length ; i++){
+	/*for(i=0; i<num.length ; i++){
 		b = b + num[i];
  }
-	ans = eval(b);
+	ans = eval(b);*/
+	ans = evaluate(num);
   displayText('screen');
 	displayText('screen',ans);
 	num = [];
 	num.push(ans.toString());
 	b = '';
+}
+
+function evaluate() { // function made to replace eval , still need to be made more efficient
+	var tmp = [];
+	var j=0;
+	var tmp2 ;
+	var x;
+	for(i=0; i<num.length ; i++) {
+		b = num[i] ;
+		if(b >= 0 && b <=9) {
+			continue;
+		}
+		else {
+			if (b == '*' || b == '/') {
+				tmp2 = 2;
+			}
+			else {
+				tmp2 = 1;
+			}
+			while(var[j] >= tmp2) {
+				if(b == '+')
+			   x = num[i-1] + num[i+1];
+			  if(b == '*')
+	      	x = num[i-1] * num[i+1];
+	    	if(b == '/')
+	        x = num[i-1] / num[i+1];
+	      if(b == '-')
+          x = num[i-1] - num[i+1];
+				num[i-1] = x;
+				num = num.splice(i , 2);
+		    i = i-1;
+			  tmp[j] = tmp2;
+		}
+		j++;
+		}
+	}
 }
 
 function clearScreen() {
